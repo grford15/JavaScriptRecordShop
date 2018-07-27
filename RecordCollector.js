a7cbc3a2f7039852ce7387385a77b1ca81635c82
Greg Rutherford <g.rutherford10@gmail.com>
2018-07-27 14:21:32 +0100

    add more passing tests

--- RecordCollector.js
+++ RecordCollector.js
@@ -1,7 +1,31 @@
 const _ = require('lodash');
 
-const RecordCollector = function() {
+const RecordCollector = function(name, wallet) {
+  this.name = name;
+  this.wallet = wallet;
+  this.collection = [];
+}
 
+RecordCollector.prototype.buyRecord = function(record) {
+  if(record.price <= this.wallet){
+    this.collection.push(record);
+    this.wallet -= record.price;
+  }
+}
+
+RecordCollector.prototype.sellRecord = function(record, buyer){
+  let selling;
+  if(this.collection.includes(record) && buyer.wallet >= record.price){
+    selling = _.remove(this.collection, function(item){
+      return item === record;
+    })
+  } buyer.collection.push(selling[0]);
+}
+
+RecordCollector.prototype.totalValueOfRecords = function(){
+  return this.collection.reduce(function(accumulator, record){
+    return accumulator += record.price;
+  }, 0)
 }
 
 module.exports = RecordCollector;
--- specs/record_collector_specs.js
+++ specs/record_collector_specs.js
@@ -1,35 +1,54 @@
 const assert = require("assert");
+const Record = require("../Record.js")
 const RecordCollector = require("../RecordCollector.js")
 
 describe("RecordCollector", function() {
-  let recordCollector;
+  let billy;
+  let alfie;
+  let blackAlbum;
+  let readyToDie;
+  let nasir;
 
   beforeEach(function() {
-    recordCollector = new RecordCollector();
+    billy = new RecordCollector("Billy", 150);
+    alfie = new RecordCollector("Alfie", 110);
+    blackAlbum = new Record("Jay-Z", "The Black Album", "Hip-Hop", 10);
+    readyToDie = new Record("Notorious BIG", "Ready to Die", "Hip-Hop", 15);
+    nasir = new Record("Nas", "NASIR", "Hip-Hop", 10);
   })
 
-  xit('should have a name', function() {
-
+  it('should have a name', function() {
+    assert.deepStrictEqual(billy.name, "Billy");
   })
 
-  xit('should have a wallet', function() {
-
+  it('should have a wallet', function() {
+    assert.deepStrictEqual(billy.wallet, 150);
   })
 
-  xit('should have a record collection', function() {
-
+  it('should have a record collection', function() {
+    assert.deepStrictEqual(billy.collection, []);
   })
 
-  xit('should be able to buy records with money, if can afford', function() {
-
+  it('should be able to buy records with money, if can afford', function() {
+    billy.buyRecord(blackAlbum);
+    billy.buyRecord(nasir);
+    assert.deepStrictEqual(billy.collection, [blackAlbum, nasir]);
+    assert.deepStrictEqual(billy.wallet, 130);
   })
 
-  xit('should be able to sell records incresing wallet', function() {
-
+  it('should be able to sell records incresing wallet', function() {
+    billy.buyRecord(blackAlbum);
+    billy.sellRecord(blackAlbum, alfie);
+    assert.deepStrictEqual(billy.collection, []);
+    assert.deepStrictEqual(alfie.collection, [blackAlbum]);
   })
 
-  xit('should be able to calcualte value of collection', function () {
-
+  it('should be able to calcualte value of collection', function () {
+    billy.buyRecord(blackAlbum);
+    billy.buyRecord(readyToDie);
+    billy.buyRecord(nasir);
+    let actual = billy.totalValueOfRecords();
+    assert.deepStrictEqual(actual, 35);
   })
 
   xit('should be able to calcualte value of records by genre', function() {
@@ -45,7 +64,7 @@ describe("RecordCollector", function() {
   })
 
   xit('should be able to compare collection', function() {
-    
+
   })
 
 })
